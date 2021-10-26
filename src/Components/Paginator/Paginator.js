3a2c4ac84d6c1a787d704a5379ce78df8544e44b
qzma82 <72132008+qzma82@users.noreply.github.com>
2021-10-26 16:01:53 +0200

    warnings

--- src/Components/Paginator/Paginator.js
+++ src/Components/Paginator/Paginator.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useCallback } from "react";
 
 const Paginator = (props) => {
-    const [activePage, setPage] = useState(1);
-    const [perPage] = useState(10);
-    const paginate = (pnum) => setPage(pnum);
-
-    const lastPost = activePage * perPage;
-    const firstPost = lastPost - perPage;
-    props.currList(props.picList.slice(firstPost, lastPost));
+    let activePage=props.activePage;
+    const picList=[...props.picList];
+    const perPage= 10;
+    let lastPost = activePage * perPage;
+    let firstPost = lastPost - perPage;
+    let curr = [...picList.slice(firstPost, lastPost)];
     const pageNum = [];
-    for(let i = 1; i <=Math.ceil(props.picList.length/perPage); i++){
-        pageNum.push(i);
+    for(let i = 1; i <=Math.ceil(picList.length/perPage); i++){
+      pageNum.push(i);
     }
-    
     const short = pageNum.slice(activePage-6>0 ? activePage-6 : 0, activePage+5<pageNum.length ? activePage+5 : pageNum.length)
-    
 
+    const fun = useCallback(() => {//fun start
+        props.dosetCurr(curr);
+    });
+    useEffect(()=>{
+            fun();
+        }, [fun]);
+    
+    
     const style1 = {
         color: 'black',
         backgroundColor: 'white',
@@ -26,14 +31,14 @@ const Paginator = (props) => {
         backgroundColor: 'grey',
       };
     return (
-        <nav><button onClick={() => paginate(activePage===1 ? activePage : activePage-1)}>previous</button>
+        <nav><button onClick={() => props.paginate(activePage===1 ? activePage : activePage-1)}>previous</button>
             {short.map(num => (
               <object key={num} style = {activePage === num ? style2:style1}>
-                <a onClick={() => paginate(num)}href="!#">{num}</a>
+                <a onClick={() => props.paginate(num)}href="!#">{num}</a>
               </object>
             ))}
-            <button onClick={() => paginate(activePage===pageNum.length ? activePage : activePage+1)}>next</button>
-            <p>Total pages: {pageNum.length} Total pic: {props.picList.length}</p>
+            <button onClick={() => props.paginate(activePage===pageNum.length ? activePage : activePage+1)}>next</button>
+            <p>Total pages: {pageNum.length} Total pic: {picList.length}</p>
         </nav>
     )
 }
